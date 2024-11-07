e9b708d1427cce2cefed6eae236fa23769da9d97
NastasKorneli <korneli.nastas@gmail.com>
2024-11-07 08:12:36 +0100

    09-11

---
+++ javascript/02-js-grundlagen/u13_running-lights/assets/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(() => {
+  // === DOM & VARS =======
+  const DOM = {};
+
+  // === INIT =============
+  const init = () => {};
+
+  // === EVENTHANDLER =====
+
+  // === XHR/FETCH ========
+
+  // === FUNCTIONS ========
+
+  init();
+})();
+
+// Übung 13: Die Rückkehr der Lauflichter: Teil 1 - Chasing Lights
+
+// Lichterketten waren einmal der letzte Schrei (und sind es um Weihnachten oft immer noch). Manche verfügen sogar über Lauflicht-Funktion. Genauso eine könntest du jetzt programmieren. Die folgende, einfache HTML-Vorlage findest du auch im Übungsmaterial:
+
+// Eine erste Variante sind die chasing Lights. Dabei muss der Anwender das Lauflicht mit der Maus »jagen«. Immer wenn der Mauszeiger sich über einer Glühbirne befindet, schaltet sich die nächste Birne ein und die aktuelle aus. Versucht der Benutzer, mit dem Mauszeiger die leuchtende Glühbirne zu berühren, springt das Licht auf die nächste über. Er »jagt« quasi das Lauflicht. Bei der letzten Lampe beginnt das Spiel wieder von vorn!
+
+// 2Programmiere ein weiteres Lauflicht. Diesmal soll das Lauflicht weiterspringen, wenn der Benutzer auf eine der Lampen klickt. Auch hier gilt: Nach der letzten Lampe fängt das Licht wieder von vorn an zu laufen.
