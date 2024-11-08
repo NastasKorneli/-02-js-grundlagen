4d4d3044ace0c24f7610c2abc34c93eafd7f9458
NastasKorneli <korneli.nastas@gmail.com>
2024-11-08 08:02:22 +0100

    14

---
+++ javascript/02-js-grundlagen/u15_tabularasa/assets/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(() => {
+  // === DOM & VARS =======
+  const DOM = {};
+
+  // === INIT =============
+  const init = () => {};
+
+  // === EVENTHANDLER =====
+
+  // === XHR/FETCH ========
+
+  // === FUNCTIONS ========
+
+  init();
+})();
+
+// Übung 15: TABula Rasa
+// Einer Ihrer Kunden ist ein Schulungsunternehmen. Der Akademieleiter wünscht sich, dass die Beschreibungsseiten der Seminare etwas übersichtlicher dargestellt werden. Daher sollen die Seminarübersichtsseiten in Zukunft über Reiter (Tabs) verfügen, sodass Besucher zwischen den Bereichen
+
+// - Überblick,
+// - Themen,
+// - Lernziele und
+// - Termine
+// hin- und herschalten können.
+
+// Eine HTML-Vorlage (inkl. CSS) hat er bereits von einer Agentur erstellen lassen. Ihre Aufgabe besteht darin, den JS-Code zu ergänzen.
+// Folgende Features müssen Sie dafür implementieren:
+
+// - Nach dem Laden müssen alle article-Elemente bis auf das erste versteckt sein.
+// - Beim Klicken auf ein li-Element soll das zugehörige article-Element aktiv (d.h sichtbar) werden und alle anderen inaktiv.
+// - Um zu sehen, welcher Tab gerade aktiv ist, darf außerdem nur das li-Element des gerade sichtbaren Tabs die CSS-Klasse active haben.
